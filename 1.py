03f09939283ff5861c4f929fe9f9ad294629786f
T-Lavany <120157371+T-Lavany@users.noreply.github.com>
2024-03-04 09:17:06 +0530

    Add files via upload

---
+++ 1.py
@@ -0,0 +1,9 @@
+import cv2 
+import numpy as np
+kernel = np.ones((5,5),np.uint8)
+print(kernel)
+path = "D:\WhatsApp Image 2024-02-22 at 14.32.08_0d8ce8d2.jpg"
+img =cv2.imread(path)
+imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+cv2.imshow("GrayScale",imgGray)
+cv2.waitKey(0)
